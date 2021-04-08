a591cfc7dff5ff5ea102a4a1775baf40d62392be
nilay1221 <nilay.sheth@somaiya.edu>
2021-04-08 16:21:00 +0530

    Added Fonts and Language Folders

---
+++ language/English.js
@@ -0,0 +1,10 @@
+module.exports = {
+    HOME :{
+        HOME : "Home",
+        EXPLORE : "Explore",
+        SUBMIT : "Submit"
+    },
+    GALLERY : "Gallery",
+    ARTISTS : "Artists",
+    CREATE : "Create",
+};
---
+++ language/Hindi.js
@@ -0,0 +1,10 @@
+module.exports = {
+    HOME :{
+        HOME : "घर",
+        EXPLORE : "खोजना",
+        SUBMIT : "प्रस्तुत"
+    },
+    GALLERY : "गेलरी",
+    ARTISTS : "कलाकार",
+    CREATE : "बनाना",
+};
